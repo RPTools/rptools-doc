9169c3bdb4b81a4a3faf4993d49dc8c41509b4d6
Craig Wisniewski <craig.wisniewski@gmail.com>
2022-07-30 16:16:08 +0930

    Document title change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Development Documentation
+            Documentation
           </Link>
         </div>
         <div className={styles.buttons}>
